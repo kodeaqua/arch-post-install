702e8000af250e4618afb92999fdffb34bbf8044
kodeaqua <anajmizdn@gmail.com>
2020-09-18 18:22:42 +0700

    Update mirror-id.sh

--- mirror-id.sh
+++ mirror-id.sh
@@ -1,1 +1,23 @@
-## Mirrors for Indonesian users
+#!/bin/bash
+# Welcome!
+echo "> Running script..."
+
+# Set var for mirrorlist
+export MIRROR=/etc/pacman.d/mirrorlist
+
+# Creating backup for original mirrorlist
+sudo mv ${MIRROR} ${MIRROR}.bak
+
+# Set for Indonesian users only
+sudo echo "## Mirrors for Indonesian users" > ${MIRROR}
+
+# Get data from original mirrorlist
+sudo cat ${MIRROR}.bak | grep ".id/" >> ${MIRROR}
+
+# Unset var
+unset MIRROR
+
+# Done
+echo "> Transaction complete!"
+echo "  Thanks for using my scripts!"
+echo "  https://github.com/kodeaqua"
